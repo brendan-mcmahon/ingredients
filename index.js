3b3652d441f87dc4ddf2b697c9e532b73e632b8b
brendanjeffreymcmahon <brendanjeffreymcmahon@gmail.com>
2023-08-05 10:44:27 -0400

    fix table name

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const db = new AWS.DynamoDB.DocumentClient();
 // Your Lambda function
 exports.handler = async (event) => {
     // Assuming the ingredient comes from event.body
+    console.log('event', event)
     const ingredient = JSON.parse(event.body);
+    console.log('ingredient', ingredient);
 
     // Construct params for DynamoDB
     const params = {
-        TableName: 'Ingredients',
+        TableName: 'ingredients',
         Item: {
             'ingredientId': ingredient.id, // assuming it has a property "id"
             'name': ingredient.name // assuming it has a property "name"
@@ -21,11 +23,13 @@ exports.handler = async (event) => {
     // Save the ingredient
     try {
         await db.put(params).promise();
+        console.log('success?');
         return {
             statusCode: 200,
             body: JSON.stringify(params.Item),
         };
     } catch (dbError) {
+        console.log('error', dbError);
         return {
             statusCode: 500,
             body: JSON.stringify(dbError),
