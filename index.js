5cc14e632b71ace17148244dbb1dc6078191c2b5
brendanjeffreymcmahon <brendanjeffreymcmahon@gmail.com>
2023-08-06 19:00:14 -0400

    Add PUT

--- index.js
+++ index.js
@@ -60,6 +60,46 @@ exports.handler = async (event) => {
         }
     }
 
+    if (event.httpMethod === 'PUT') {
+        const ingredient = JSON.parse(event.body);
+        console.log('ingredient', ingredient);
+
+        const params = {
+            TableName: 'ingredients',
+            Key: {
+                'ingredientId': ingredient.id
+            },
+            UpdateExpression: 'set #name = :name, #tags = :tags, #status = :status',
+            ExpressionAttributeNames: {
+                '#name': 'name',
+                '#tags': 'tags',
+                '#status': 'status',
+            },
+            ExpressionAttributeValues: {
+                ':name': ingredient.name,
+                ':tags': ingredient.tags,
+                ':status': ingredient.status,
+            },
+            ReturnValues: 'ALL_NEW'
+        };
+
+        try {
+            const result = await db.update(params).promise();
+            return {
+                statusCode: 200,
+                headers,
+                body: JSON.stringify(result.Attributes),
+            };
+        } catch (dbError) {
+            console.log('error', dbError);
+            return {
+                statusCode: 500,
+                headers,
+                body: JSON.stringify(dbError),
+            };
+        }
+    }
+
     // Handle POST request
     if (event.httpMethod === 'POST') {
         const ingredient = JSON.parse(event.body);
