3797b5c81d643bbd6caaaab24edef946108195f4
brendanjeffreymcmahon <brendanjeffreymcmahon@gmail.com>
2023-08-05 23:38:11 -0400

    CORS

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-// Import AWS SDK
 const AWS = require('aws-sdk');
 
-// Create DynamoDB service object
 const db = new AWS.DynamoDB.DocumentClient();
 
-// Your Lambda function
 exports.handler = async (event) => {
-    // Assuming the ingredient comes from event.body
-    console.log('event', event)
+    console.log('event', event);
     const ingredient = JSON.parse(event.body);
     console.log('ingredient', ingredient);
-
-    // Construct params for DynamoDB
+    
     const params = {
         TableName: 'ingredients',
         Item: {
-            'ingredientId': ingredient.id, // assuming it has a property "id"
-            'name': ingredient.name // assuming it has a property "name"
+            'ingredientId': ingredient.id, 
+            'name': ingredient.name 
         }
     };
-
-    // Save the ingredient
+    
     try {
         await db.put(params).promise();
         console.log('success?');
+        
         return {
             statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*", // Adjust this for production!
+                "Access-Control-Allow-Credentials": true
+            },
             body: JSON.stringify(params.Item),
         };
     } catch (dbError) {
         console.log('error', dbError);
         return {
             statusCode: 500,
+            headers: {
+                "Access-Control-Allow-Origin": "*", // Adjust this for production!
+                "Access-Control-Allow-Credentials": true
+            },
             body: JSON.stringify(dbError),
         };
     }
